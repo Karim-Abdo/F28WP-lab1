31eeac46ae2d0a503e9e268c83289d7d3b031a0d
Karim-Abdo <145095641+Karim-Abdo@users.noreply.github.com>
2023-11-03 18:14:03 +0400

    Update main.js

--- week4/main.js
+++ week4/main.js
@@ -1,6 +1,17 @@
+var cityContainer = document.getElementById("city-info");
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
+
+  
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://github.com/Karim-Abdo/F28WP-lab1/blob/main/week4/cities1.json');
 ourRequest.onload = function() {
 console.log(ourRequest.responseText);
 };
 ourRequest.send();
+
+});
+function renderHTML(data){
+var htmlString = "this is a test";
+cityContainer.insertAdjacentHTML('beforeend' , htmlString);
+}
